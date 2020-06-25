59d3885193cef8e9c7b3345e0872f45f2d315c64
Jannung Kusdiantoro <iceboltz@gmail.com>
2020-06-25 21:25:35 +0700

    Initial Create

---
+++ SSST/Models/MataPelajaran.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSST.Models
+{
+    public class MataPelajaran
+    {
+        public int MapelID { get; set; }
+        [Display(Name ="Nama Pelajaran")]
+        public string MapelNama { get; set; }
+    }
+}
---
+++ SSST/Models/Siswa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSST.Models
+{
+    public class Siswa
+    {
+        public int SiswaID { get; set; }
+        public string SiswaNim { get; set; }
+        [Display(Name ="Nama Siswa")]
+        public string SiswaNama { get; set; }
+        [Display(Name ="Alamat")]
+        public string SiswaAlamat { get; set; }
+
+    }
+}
